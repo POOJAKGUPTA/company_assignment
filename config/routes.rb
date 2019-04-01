0b59b038db4211858ac8ed42147cb130d78c14c5
poojak gupta <poojakgupta4@gmail.com>
2019-04-01 20:15:52 +0530

    active admin now work good

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
+  resources :users
+  
   resources :companies
   resources :places
   resources :nodes
